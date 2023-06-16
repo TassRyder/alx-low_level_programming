445963363c609f79ca4b4dff3ed70e74cb3c95a0
TassRyder <ojames1301@gmail.com>
2023-06-16 03:47:11 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio_h>
+/**
+ * main - A program that print aline using the printf function
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n")
+	return (0);
+}
