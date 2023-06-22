0cc51b7f4c5d783d9c3619c795a80bc4084f43fc
TassRyder <ojames1301@gmail.com>
2023-06-22 11:09:53 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a staright line according to parameter
+ * @n: The numnber of lines to draw
+ * Return: empty
+ */
+
+void print_line(int n)
+
+{
+	int x;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < n; X++)
+	{
+	_putchar(95);
+	}
+	_putchar('\n');
+	}
+}
