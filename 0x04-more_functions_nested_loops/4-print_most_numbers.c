a003a57e28ba4790a291c6b4ea0db8777c747846
TassRyder <ojames1301@gmail.com>
2023-06-22 10:51:02 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,12 +6,12 @@
  * Return: The numbers since 0 up to 9
  */
 
-void prints_most_numbers(void)
+void print_most_numbers(void)
 
 {
 	int x = 0;
 
-	for (; x <= 9; x++)
+	for (; x <= 9; X++)
 	{
 	if (x == 2 || x == 4)
 	{
