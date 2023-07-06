8b969b8cfd12d72e03924b1d4b8e28024b5d1d1b
TassRyder <ojames1301@gmail.com>
2023-07-06 07:04:50 -0700

    0x08-recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Print a string in revers
+ * _print_rev_recursion - Print a string in reverse
  * @s: the string to reverse
  *
  * Return: Nothing
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == ('\0')
+	if (*s == '\0')
 	{
 		return;
 	}
