b1a5e4e75b9efac0fec8bde335940bbb89e4e98f
TassRyder <ojames1301@gmail.com>
2023-07-11 06:14:21 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * main - Prints all the args content
- * @argc: argument count
- * @argv: argument vector
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: Always zero
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
@@ -19,3 +19,4 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
+
