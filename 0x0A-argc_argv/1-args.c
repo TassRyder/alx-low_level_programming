6b414c907fe6a57a2f81fafbd91b8dc00b49b0c5
TassRyder <ojames1301@gmail.com>
2023-07-11 05:17:40 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -11,6 +10,8 @@
 int main(int argc, char *argv[])
 {
 	(void) argv;
-	printf("%d\n", argc - 1);
+	printf("%d\n", argv - 1);
+
 	return (0);
 }
+
